59f22734ba996f6cb940d3f729c0f7c2fd61acaf
Jafid Garcia <jafidgarcia@gmail.com>
2022-09-30 16:01:41 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h2>{{title}}</h2>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data (){
+    return{
+      title: 'Hello World'
+    }
   }
 }
 </script>
@@ -24,5 +27,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h2{
+  color: green;
 }
 </style>
